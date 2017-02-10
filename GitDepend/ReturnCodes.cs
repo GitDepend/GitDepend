ad6e6c3ac49f4f92e43a5d4a09857f4afc681b77
Kevin Johnson <kljohnson@xactware.com>
2017-02-10 16:13:40 -0700

    closes #5
    the update flag will now run the build script for each dependency

--- GitDepend/ReturnCodes.cs
+++ GitDepend/ReturnCodes.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GitDepend
+﻿namespace GitDepend
 {
 	public static class ReturnCodes
 	{
 		public const int Success = 0;
 		public const int GitDependFileNotFound = 1;
 		public const int GitRepositoryNotFound = 2;
+		public const int FailedToRunGitCommand = 3;
+		public const int FailedToRunBuildScript = 4;
 	}
 }
+	
---
+++ GitDepend/Visitors/DependencyVisitorAlgorithm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using GitDepend.Configuration;
+
+namespace GitDepend.Visitors
+{
+	public class DependencyVisitorAlgorithm
+	{
+		public void TraverseDependencies(IVisitor visitor, string directory)
+		{
+			directory = Path.GetFullPath(directory);
+
+			if (!Directory.Exists(directory))
+			{
+				visitor.ReturnCode = ReturnCodes.GitRepositoryNotFound;
+				return;
+			}
+
+			string dir;
+			string error;
+			var config = GitDependFile.LoadFromDir(directory, out dir, out error);
+
+			if (config == null)
+			{
+				Console.Error.WriteLine("Could not find GitDepend.json");
+				visitor.ReturnCode = ReturnCodes.GitDependFileNotFound;
+				return;
+			}
+
+			foreach (var dependency in config.Dependencies)
+			{
+				int code = ReturnCodes.Success;
+				dependency.Directory = Path.GetFullPath(Path.Combine(dir, dependency.Directory));
+
+				if (!Directory.Exists(dependency.Directory))
+				{
+					Console.WriteLine($"Cloning {dependency.Name} into {dependency.Directory}");
+					var git = new Git();
+
+					code = git.Clone(dependency.Url, dependency.Directory, dependency.Branch);
+					Console.WriteLine();
+					if (code != ReturnCodes.Success)
+					{
+						visitor.ReturnCode = code;
+						return;
+					}
+				}
+
+				TraverseDependencies(visitor, dependency.Directory);
+
+				code = visitor.VisitDependency(dependency);
+				Console.WriteLine();
+
+				if (code != ReturnCodes.Success)
+				{
+					return;
+				}
+			}
+		}
+	}
+}

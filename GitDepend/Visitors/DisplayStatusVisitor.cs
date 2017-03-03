8b19c5dd73508bd3bb28d3f7d3c82e82e66e7efc
Kevin Johnson <kljohnson@xactware.com>
2017-03-02 17:33:24 -0700

    issue #138 Fixes status verb
    Why:
    
    * The status verb was incorrectly calculating the working directory for git
    
    This change addresses the need by:
    
    * combining the project directory with the dependency directory, rather than taking the project directoy only.

--- GitDepend/Visitors/DisplayStatusVisitor.cs
+++ GitDepend/Visitors/DisplayStatusVisitor.cs
@@ -11,6 +11,7 @@ namespace GitDepend.Visitors
     public class DisplayStatusVisitor : NamedDependenciesVisitor
     {
         private readonly IGit _git;
+        private readonly IFileSystem _fileSystem;
 
         /// <summary>
         /// Creates a new <see cref="DisplayStatusVisitor"/>
@@ -19,6 +20,7 @@ namespace GitDepend.Visitors
         public DisplayStatusVisitor(IList<string> whitelist) : base(whitelist)
         {
             _git = DependencyInjection.Resolve<IGit>();
+            _fileSystem = DependencyInjection.Resolve<IFileSystem>();
         }
 
         #region Overrides of NamedDependenciesVisitor
@@ -31,7 +33,8 @@ namespace GitDepend.Visitors
         /// <returns>The return code.</returns>
         protected override ReturnCode OnVisitDependency(string directory, Dependency dependency)
         {
-            _git.WorkingDirectory = directory;
+            var path = _fileSystem.Path.GetFullPath(_fileSystem.Path.Combine(directory, dependency.Directory));
+            _git.WorkingDirectory = path;
             return ReturnCode = _git.Status();
         }
 

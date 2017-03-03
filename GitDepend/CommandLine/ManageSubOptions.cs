6ae366c29182a91d8432d14ac8c8122f7f1384b7
Preston Williams <prwilliams@xactware.com>
2017-03-03 13:32:36 -0700

    issue #148 Adding new basic classes for managing dependencies
    Why:
    
    * We need to add another command to be able to manage gitdepend.json
    
    This change addresses the need by:
    
    * Creating basic classes for the new command

---
+++ GitDepend/CommandLine/ManageSubOptions.cs
@@ -0,0 +1,48 @@
+﻿using CommandLine;
+
+
+namespace GitDepend.CommandLine
+{
+    /// <summary>
+    /// Contains the options for the Manage command.
+    /// </summary>
+    /// <seealso cref="GitDepend.CommandLine.CommonSubOptions" />
+    public class ManageSubOptions : CommonSubOptions
+    {
+        /// <summary>
+        /// Gets or sets the name of the dependency.
+        /// </summary>
+        [Option("depname", Required= false, HelpText = "The name of the dependency you want to update/remove. Leave out if you want to apply to all dependencies.")]
+        public string DependencyName { get; set; }
+
+        /// <summary>
+        /// Adds a dependency to the GitDepend.json file, based on the other dependencyname.
+        /// </summary>
+        [Option("add", Required = false, HelpText = "This is to add a dependency to the current directory or the dependency named.")]
+        public string Add { get; set; }
+
+        /// <summary>
+        /// Removes a dependencyin the GitDepend.json file, based on the dependencyname.
+        /// </summary>
+        [Option("remove", Required = false, HelpText = "Removes the dependency given by its name. If no name is provided, no deletion occurs")]
+        public string Remove { get; set; }
+
+        /// <summary>
+        /// Sets the url for the given dependency in the GitDepend.json file.
+        /// </summary>
+        [Option("url", Required = false, HelpText = "Sets the url for the given dependency.")]
+        public string SetUrl { get; set; }
+
+        /// <summary>
+        /// Sets the directory for the given dependency in the GitDepend.Json file.
+        /// </summary>
+        [Option("dir", Required = false, HelpText = "Sets the directory for the given dependency.")]
+        public string SetDirectory { get; set; }
+
+        /// <summary>
+        /// Sets the branch for the given dependency, or updates all.
+        /// </summary>
+        [Option("branch", Required = false, HelpText = "Sets the branch for the given named dependency, or updates all if no depname is given")]
+        public string SetBranch { get; set; }
+    }
+}
---
+++ GitDepend/Commands/Manage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitDepend.Busi;
+using GitDepend.CommandLine;
+
+namespace GitDepend.Commands
+{
+    /// <summary>
+    /// This command lets you manage the dependencies and their configuration files.
+    /// </summary>
+    public class Manage : ICommand
+    {
+        /// <summary>
+        /// The name of the verb.
+        /// </summary>
+        public const string Name = "manage";
+
+        private readonly ManageSubOptions _options;
+        private readonly IGitDependFileFactory _factory;
+        private readonly IConsole _console;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Manage"/> class.
+        /// </summary>
+        /// <param name="options">The <see cref="ManageSubOptions"/> that configure manage</param>
+        public Manage(ManageSubOptions options)
+        {
+            _options = options;
+            _factory = DependencyInjection.Resolve<IGitDependFileFactory>();
+            _console = DependencyInjection.Resolve<IConsole>();
+
+        }
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public ReturnCode Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
